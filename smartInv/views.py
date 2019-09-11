adddfe412b7d461299571a00599f89e5d35eaa68
keenewong <keenewonghofung@gmail.com>
2019-09-11 16:43:30 -0400

    grant permission

--- smartInv/views.py
+++ smartInv/views.py
@@ -7,14 +7,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ItemList(generics.ListCreateAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 
 class UserItemList(generics.ListAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = ItemSerializer
 
     def get_queryset(self):
@@ -24,21 +24,21 @@ class UserItemList(generics.ListAPIView):
 
 
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 
 class CatergoryList(generics.ListCreateAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Catergory.objects.all()
     serializer_class = CatergorySerializer
 
 
 class CatergoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Catergory.objects.all()
     serializer_class = CatergorySerializer
