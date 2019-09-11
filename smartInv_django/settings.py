a2eaf6d7abea62249cbfd0eb4e01f70754b6382b
keenewong <keenewonghofung@gmail.com>
2019-09-11 16:46:34 -0400

    grant permission

--- smartInv_django/settings.py
+++ smartInv_django/settings.py
@@ -135,7 +135,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAdminUser'
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'
